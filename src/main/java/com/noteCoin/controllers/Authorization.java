26b2f83b7d30fc8c722da9a49466d311c36bc6b7
Dmitry-Ukhin <silentium.noxe@gmail.com>
2018-04-20 23:51:59 +0300

    trying to add OAuth2 for Google

--- src/main/java/com/noteCoin/controllers/Authorization.java
+++ src/main/java/com/noteCoin/controllers/Authorization.java
@@ -1,59 +1,38 @@
 package com.noteCoin.controllers;
 
-import com.noteCoin.data.UserDAOHibernate;
-import com.noteCoin.data.dao.UserDAO;
-import com.noteCoin.models.User;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 
-import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-public class Authorization extends HttpServlet{
+import com.noteCoin.tools.IdTokenVerifierAndParser;
+
+public class Authorization extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String pass = req.getParameter("key");
-        Integer int_hashPass = pass.hashCode();
-        String str_hashPass = int_hashPass.toString();
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-        HttpSession session = req.getSession();
-        String sessionId = session.getId();
+        resp.setContentType("text/html");
 
-        Date date = new Date();
+        try {
+            String idToken = req.getParameter("id_token");
+            GoogleIdToken.Payload payLoad = IdTokenVerifierAndParser.getPayload(idToken);
+            String name = (String) payLoad.get("name");
+            String email = payLoad.getEmail();
+            System.out.println("User name: " + name);
+            System.out.println("User email: " + email);
 
-        String query = "Select * From Users WHERE name=\'" + name + "\' AND pass=" + str_hashPass;
+            HttpSession session = req.getSession(true);
+            session.setAttribute("userName", name);
+            req.getServletContext()
+                    .getRequestDispatcher("/welcome-page.jsp").forward(req, resp);
 
-        UserDAO userDAO = new UserDAOHibernate();
-        List<User> userList = userDAO.getList(query);
-        if (null != userList && userList.size() == 1){
-            userList.get(0).setLastEnter(date);
-            User update = userDAO.update(userList.get(0));
-            if (null != update) {
-                resp.getWriter().println("STAT: 200");
-            }else{
-                resp.getWriter().println("STAT: 501");
-            }
-        }else{
-            Integer size;
-            if (userList != null){
-                size = userList.size();
-            }else{
-                size = null;
-            }
-
-            resp.getWriter().println("STAT: 403//" + size);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
-
     }
 }
